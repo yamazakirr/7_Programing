770500ee1e377c20771ae22e128543a13f9b0e8b
yamazakirr <yamazaki0317@icloud.com>
2022-08-25 22:48:56 +0900

    2022/8/25

---
+++ account/sql/account.sql
@@ -0,0 +1,27 @@
+set names utf8;
+set foreign_key checks = 0;
+drop database if exists account;
+create database if not exists account;
+use account;
+
+drop table if exists login_user_transaction;
+
+create table login_user_transaction(
+id int(100),
+family_name varchar(100),
+last_name varchar(100),
+family_name_kana varchar(100),
+last_name_kana varchar(100),
+mail varchar(255),
+password varchar(255),
+gender int(1),
+postal_code varchar(100),
+prefecture varchar(100),
+address_1 varchar(100),
+address_2 varchar(255),
+authority int(1),
+delete_flg int(1),
+registered_time datetime,
+update_time datetime
+
+);
---
+++ account/src/com/diworksdev/account/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author yamazakirikuo
+ *
+ */
+package com.diworksdev.account.util;
